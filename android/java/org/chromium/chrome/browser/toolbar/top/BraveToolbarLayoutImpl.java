e836d1a7c99cf2daf48a76038a30649e8dc96ddb
deeppandya <deeppandya91@gmail.com>
2024-05-27 13:35:19 -0400

    Add changes for player notification click

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -671,7 +671,9 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                                         == PlaylistOptionsEnum.OPEN_PLAYLIST) {
                                     BraveActivity.getBraveActivity()
                                             .openPlaylistActivity(
-                                                    getContext(), ConstantUtils.DEFAULT_PLAYLIST);
+                                                    getContext(),
+                                                    ConstantUtils.DEFAULT_PLAYLIST,
+                                                    false);
                                 } else if (playlistOptionsModel.getOptionType()
                                         == PlaylistOptionsEnum.PLAYLIST_SETTINGS) {
                                     BraveActivity.getBraveActivity().openBravePlaylistSettings();
@@ -760,7 +762,9 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                                 try {
                                     BraveActivity.getBraveActivity()
                                             .openPlaylistActivity(
-                                                    getContext(), ConstantUtils.DEFAULT_PLAYLIST);
+                                                    getContext(),
+                                                    ConstantUtils.DEFAULT_PLAYLIST,
+                                                    false);
                                 } catch (BraveActivity.BraveActivityNotFoundException e) {
                                     Log.e(TAG, "showAddedToPlaylistSnackBar onClick ", e);
                                 }
