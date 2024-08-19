3be5aa38920383485276cca889f41f8dee2295a5
deeppandya <deeppandya91@gmail.com>
2024-08-20 02:10:47 +0530

    Add kotlin files patch

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -721,7 +721,6 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                             playlistItems.add(playlistItem);
                         }
                     }
-                    // if (playlistItems.size() > 0) {
                     mPlaylistService.addMediaFiles(
                             playlistItems.toArray(new PlaylistItem[0]),
                             ConstantUtils.DEFAULT_PLAYLIST,
@@ -731,9 +730,6 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                                     showAddedToPlaylistSnackBar();
                                 }
                             });
-                    // } else {
-                    //     showAlreadyAddedToPlaylistSnackBar();
-                    // }
                 });
     }
 
